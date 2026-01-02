ea8d731ad96d91e62d5120071a6e88195503f51d
Pankaj Kumar Gupta <140154353+pankajkrg@users.noreply.github.com>
2026-01-02 15:05:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask Application - Mr. Raghu"
+    return "Welcome to Python Flask Application - Mr. Pankaj Gupta"
 
 
 if __name__ == "__main__":
